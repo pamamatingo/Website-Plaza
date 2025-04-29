9ff1a212382d934f2d0cd67018c72342de0ed6f9
noel <Noelivanperez82@gmail.com>
2025-04-29 02:07:07 -0400

    agregando herramienta administrativa y refactorizando el codigo

---
+++ assets/js/aos-init.js
@@ -0,0 +1,7 @@
+// aos-init.js
+
+// Inicializar AOS (Animate On Scroll)
+AOS.init({
+    duration: 1000, // Duración de las animaciones
+    once: true      // Solo animar una vez al hacer scroll
+});
